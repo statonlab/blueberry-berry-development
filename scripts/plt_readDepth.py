25ed9adc875adaf91b0e8978478f780540ca3d99
jiali0411 <jyu28@vols.utk.edu>
2020-07-26 17:50:01 -0400

    read depth plot

---
+++ scripts/plt_readDepth.py
@@ -0,0 +1,26 @@
+import os
+import matplotlib as matpl
+if os.environ.get('DISPLAY','') == '':
+    print('Currently no display found. Using the non-interactive Agg backend')
+    matpl.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sys
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+data1 = pd.read_table(file1, sep='\t', names=['chr','pos','depth'])
+data2 = pd.read_table(file2, sep='\t', names=['chr','pos','depth'])
+x1 = data1['pos'].values
+y1 = data1['depth'].values
+x2 = data2['pos'].values
+y2 = data2['depth'].values
+
+fig,axs = plt.subplots(2)
+axs[0].plot(x1,y1)
+axs[0].set(ylabel='read depth')
+axs[1].plot(x2,y2)
+axs[1].set(xlabel='position',ylabel='read depth')
+
+plt.savefig("lines.png")
