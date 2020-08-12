66d0227e1b369853629a946a0338d7fef96f1b63
jiali0411 <jyu28@vols.utk.edu>
2020-08-12 11:08:51 -0400

    edit protein fasta

---
+++ scripts/add_asterisk_stop_codon.py
@@ -0,0 +1,20 @@
+"""
+Usage python add_asterisk_stop_codon.py <input fasta> <output fasta>
+"""
+import sys
+from Bio.Seq import Seq
+from Bio import SeqIO
+
+fasta_in = sys.argv[1]
+fasta_out = sys.argv[2]
+
+def add_stop_codon(record):
+    return SeqRecord(seq=record.seq+"*", \
+            id=record.id, \
+            description="")
+
+fasta_seq = SeqIO.parse(open(fasta_in), "fasta")
+with open(fasta_out, "w") as out:
+    for seq in fasta_seq:
+        seq_with_stop = add_stop_codon(seq)
+        SeqIO.write(seq_with_stop, out, "fasta")
