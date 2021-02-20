93b461c1280bc67041f80de478c402bf8a523886
jiali0411 <jyu28@vols.utk.edu>
2021-02-19 23:04:15 -0500

    update codes

--- Rcodes/filter_gene2genome.R
+++ Rcodes/filter_gene2genome.R
@@ -5,7 +5,7 @@ library(plyr)
 blast <- read.table("../results/orthogroups/VdGene2Genome.blast", header = F, sep = "\t")
 length(unique(blast$qseqid))
 names(blast) <- c("qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore")
-blast_filter <- blast[blast$length > 150, ]
+blast_filter <- blast[blast$length > 200, ]
 
 order_evalue <- blast_filter[order(blast_filter$evalue, decreasing = F),]
 order_evalue$pair <- paste0(order_evalue$qseqid,"_",order_evalue$sseqid)
@@ -20,3 +20,12 @@ match_Genes <- uniq_blast[uniq_blast$qseqid %in% rbh$V2,]
 length(unique(match_Genes$qseqid))
 unmatch_Vd_genes <- uniq_blast[!(uniq_blast$qseqid %in% rbh$V2),]
 length(unique(unmatch_Vd_genes$qseqid))
+
+# output genes
+matched_vdIDs<- unique(match_Genes[,2, drop=F])
+matched_vdIDs <- gsub("ID=","",matched_vdIDs$qseqid)
+write.table(matched_vdIDs,"../results/orthogroups/no_orthgroup_Vd_matched_VcGenes.txt", quote = F, row.names = F, col.names = F)
+
+unmatched_VdIDs <- unique(unmatch_Vd_genes[,2,drop=F])
+unmatched_VdIDs <- gsub("ID=","",unmatched_VdIDs$qseqid)
+write.table(unmatched_VdIDs,"../results/orthogroups/no_orthgroup_Vd_unmatched_VcGenes.txt", quote = F, row.names = F, col.names = F)
